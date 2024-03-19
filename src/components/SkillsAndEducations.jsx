a9e1472c65c38e4fbb76404e39b01beb3182607e
Haiden McGill <46386953+heyhaiden@users.noreply.github.com>
2024-03-19 12:28:53 +0000

    Update SkillsAndEducations.jsx

--- src/components/SkillsAndEducations.jsx
+++ src/components/SkillsAndEducations.jsx
@@ -3,12 +3,12 @@ import {useState} from "react";
 const SkillsAndEducations = () => {
   const [tab, setTab] = useState('Services')
 
-  // const allTabs = ['Services', 'Education', 'Certifications']
+  //const allTabs = ['Services', 'Education', 'Certifications']
   const tabInfo = {
-    'Services': 'Ecological Consultancy, Biodiversity Management, Species Monitoring, Ecological Surveys, IoT Monitoring Development, GIS Mapping, Sustainable Design Consulting, Clerk of Works Services, Environmental Compliance Reports, Project Management',
-    'Education': 'MSc Connected Environments, University College London, 2023, Post-graduate Diploma in Landscape Ecology with GIS, University of Greenwich, 2015, BSc Landscape Management (Land Use), University of Greenwich, 2013',
-    'Certifications': 'QGIS and LandAPP, UAV operator (A2 CofC certified and insured), CSCS card, valid until June 2024, Natural England Level 1 bat and great crested newt holder, Outdoor First Aid Trained'
-  };
+    'Services': ['Ecological Consultancy, Biodiversity Management, Species Monitoring, Ecological Surveys, IoT Monitoring Development, GIS Mapping, Sustainable Design Consulting, Clerk of Works Services, Environmental Compliance Reports, Project Management'],
+    'Education': ['MSc Connected Environments, University College London, 2023, Post-graduate Diploma in Landscape Ecology with GIS, University of Greenwich, 2015, BSc Landscape Management (Land Use), University of Greenwich, 2013'],
+    'Certifications': ['QGIS and LandAPP, UAV operator (A2 CofC certified and insured), CSCS card, valid until June 2024, Natural England Level 1 bat and great crested newt holder, Outdoor First Aid Trained']
+  }
   
   const renderRespectiveTabBody = () => { 
     const options = tabInfo[tab]
